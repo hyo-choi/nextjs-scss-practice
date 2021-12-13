111c9822ef3f79c3a27ab73a0b63d6c8da6360da
hyo-choi <hyochoi31@gmail.com>
2021-12-13 11:56:50 +0900

    :sparkles: Nav 컴포넌트 작성

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Link from 'next/link';
+import styles from './Nav.module.scss';
+
+const Nav = () => (
+  <nav className={styles.nav}>
+    <Link href="/"><a className={styles.title}>Blog</a></Link>
+    <ul>
+      <li><Link href="/"><a>menu</a></Link></li>
+      <li><Link href="/"><a>menu</a></Link></li>
+      <li><Link href="/"><a>menu</a></Link></li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
