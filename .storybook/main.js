1753abc19be258359acefb842db74d53fa182bdd
hyo-choi <hyochoi31@gmail.com>
2021-12-14 18:39:27 +0900

    :heavy_plus_sign: Storybook 추가

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    "../**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  }
+}
