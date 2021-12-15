d2bef4e2810f78f150a472729fde534a340bb508
hyo-choi <hyochoi31@gmail.com>
2021-12-15 10:43:17 +0900

    :heavy_plus_sign: storybook-addon-a11y 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-scss"
+    "@storybook/preset-scss",
+    "@storybook/addon-a11y"
   ],
   "framework": "@storybook/react",
   "core": {
